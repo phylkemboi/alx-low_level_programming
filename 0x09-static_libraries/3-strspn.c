d9a2c641944ae8a752502ca7d97590483985f5c6
philip kemboi <phillkemboi4576@gmail.com>
2023-10-02 02:15:15 -0700

    kemboi

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,26 +1,31 @@
 #include "main.h"
+
 /**
-* _strspn - Entry point
-* @s: input
-* @accept: input
-* Return: Always 0 (Success)
-*/
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Number of bytes in the initial segment of s which consist
+ * only of bytes from accept
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int n = 0;
-int r;
-while (*s)
-{
-for (r = 0; accept[r]; r++)
-{
-if (*s == accept[r])
-{
-n++;
-break;
-}
-else if (accept[r + 1] == '\0')
-return (n);
-}
-s++;
+    unsigned int n = 0;
+    int r;
+
+    while (*s)
+    {
+        for (r = 0; accept[r]; r++)
+        {
+            if (*s == accept[r])
+            {
+                n++;
+                break;
+            }
+            else if (accept[r + 1] == '\0')
+                return (n);
+        }
+        s++;
+    }
+    return (n);
 }
 
