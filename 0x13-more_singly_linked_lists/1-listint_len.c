73b294577a1d316fa19ce2542de1bf93c28caf79
phylkemboi <phillkemboi4576@gmail.com>
2023-10-23 09:17:39 -0700

    kemboi

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * listint_len - a function that returns the number of elements.
+ * @h: pointer to the first node
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 1;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		if (h->n != '\0')
+			count++;
+
+		h = h->next;
+	}
+
+	return (count);
+}
